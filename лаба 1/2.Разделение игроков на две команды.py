3c4151e30b9bb1d6f6abfaa66e3c8b907d94c533
Mozgich <148548009+Mozgich@users.noreply.github.com>
2023-10-28 13:01:07 +0300

    Add files via upload

---
+++ лаба 1/2.Разделение игроков на две команды.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+total_players = len(list_players)
+first_team = list_players[0:3]
+second_team = list_players[3:]
+# TODO Разделите участников на две команды
+print(first_team)
+print(second_team)
