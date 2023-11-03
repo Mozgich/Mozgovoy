dd7f1e9649d84438951a220b3758a903d0fc3200
Mozgich <148548009+Mozgich@users.noreply.github.com>
2023-11-03 13:11:43 +0300

    Update 2.Разделение игроков на две команды.py

--- лаба 1/2.Разделение игроков на две команды.py
+++ лаба 1/2.Разделение игроков на две команды.py
@@ -1,7 +1,8 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+middle = len(list_players) // 2
 total_players = len(list_players)
-first_team = list_players[0:3]
-second_team = list_players[3:]
+first_team = list_players[0:middle]
+second_team = list_players[middle:]
 # TODO Разделите участников на две команды
 print(first_team)
 print(second_team)
