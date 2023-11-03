545e810993a43e28842d9a16b4e072360f70090e
Mozgich <148548009+Mozgich@users.noreply.github.com>
2023-11-03 13:10:43 +0300

    Update 1.Замена пропущенного элемента средним арифметическим.py

--- лаба 1/1.Замена пропущенного элемента средним арифметическим.py
+++ лаба 1/1.Замена пропущенного элемента средним арифметическим.py
@@ -1,8 +1,9 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
-summ = sum(numbers[: 4]) + sum(numbers[5:])
+missing_item = 4
+summ = sum(numbers[: missing_item]) + sum(numbers[missing_item + 1:])
 len_ = len(numbers)
 ср_ар_ = summ / len_
-numbers[4] = ср_ар_
+numbers[missing_item] = ср_ар_
 print("Измененный список:", numbers)
