52ec5d660ae5ca40080512faaad79271f31069c5
Mozgich <148548009+Mozgich@users.noreply.github.com>
2023-11-15 23:47:07 +0300

    Update 1.Поиск индекса элемента в списке товаров.py

--- лаба 3/1.Поиск индекса элемента в списке товаров.py
+++ лаба 3/1.Поиск индекса элемента в списке товаров.py
@@ -1,10 +1,10 @@
 # TODO Напишите функцию find_common_items
 def find_common_items(a, b):
-    общие = list(set(a).intersection(b))
-    общие.sort()
-    return общие
+    common = list(set(a).intersection(b))
+    common.sort()
+    return common
 
 last_week_items = ['книга', 'ноутбук', 'флешка', 'мышь']
 current_week_items = ['ноутбук', 'флешка', 'наушники', 'монитор']
-надо = find_common_items(last_week_items, current_week_items)
-print(f"Общие товары: {надо}")  # TODO Распечатайте общие товары
+current_items = find_common_items(last_week_items, current_week_items)
+print(f"Общие товары: {current_items}")  # TODO Распечатайте общие товары
